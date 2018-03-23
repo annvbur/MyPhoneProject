79bd86438d5647f7bdd1274e1f333777860e46dc
Hanna Burianova <annvbur@gmail.com>
2018-03-23 12:13:33 +0200

    Screenshot and Swipe

--- src/main/java/utils/Swipe.java
+++ src/main/java/utils/Swipe.java
@@ -6,14 +6,14 @@ import io.appium.java_client.TouchAction;
 import static java.time.Duration.ofMillis;
 
 public class Swipe {
-    public void down(AppiumDriver driver)
+    public static void down(AppiumDriver driver)
     {
         int height = driver.manage().window().getSize().height;
         int width = driver.manage().window().getSize().width;
 
         new TouchAction(driver)
                 .press(width / 2, height / 2)
-                .waitAction(ofMillis(0))
+                .waitAction(ofMillis(3000))
                 .moveTo(width / 2, -(height + height) / 2)
                 .release()
                 .perform();
